71ee234d12da3104fbdf4d0905164993f742dcfd
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-05-07 10:35:11 +0200

    add dockerfile

--- server/src/services/youtubeDl.js
+++ server/src/services/youtubeDl.js
@@ -1,5 +1,5 @@
 const getDownloadCommand = (videoUrl, outputPath) => {
-	return `yt-dlp --no-check-certificate -f bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best -o "${outputPath}" "${videoUrl}"  --progress-delta 0.1`;
+	return `yt-dlp --no-check-certificate -f bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best -o "${outputPath}" "${videoUrl}"`;
 };
 
 module.exports = { getDownloadCommand };
